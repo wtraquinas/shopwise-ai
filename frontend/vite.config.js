28c348c28cce581d0dc31f4ae0482c8da72fb970
Antonio Trx <wtraquinas@gmail.com>
2026-07-24 12:29:19 +0100

    Fix Vite output directory for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-
   build: {
-    outDir: path.resolve(__dirname, "../dist"),
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
